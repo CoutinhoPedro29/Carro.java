93e8ff6a0ab5313cbc77e377b0d2d35ff68cc0bf
CoutinhoPedro29 <57445134+CoutinhoPedro29@users.noreply.github.com>
2021-03-24 14:37:27 -0300

    Create TesterCarro.java

---
+++ TesterCarro.java
@@ -0,0 +1,11 @@
+public class TesterCarro estends Carro 
+{
+public static void main(String[] args)
+{
+Bmw bmw= new Bmw();
+Bmw.velocidade=260;
+Carro carro=new Carro();
+Carro.num_acento=250;
+carro.cor="vermoelho"
+}
+}
